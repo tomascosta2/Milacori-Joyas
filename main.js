e234ebdf154375ca79f93ad741d248c1fcc605d1
Tomas Costa <tomascostapp@gmail.com>
2022-05-26 15:57:31 -0300

    Commit Form

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const openCloseButton = document.querySelector('#buttonOpenCloseMenu');
 const menuMobileItems = document.querySelector('#menu-mobile-items');
 
+const enviarBtn = document.querySelector(".enviar");
+const enviado = document.querySelector(".enviado");
+
 openCloseButton.addEventListener('click', e => {
     menuMobileItems.classList.toggle('menu-mobile-closed')
+});
+
+enviarBtn.addEventListener('click', e => {
+    e.preventDefault();
+    enviado.innerHTML = "Mensaje enviado correctamente";
 });
